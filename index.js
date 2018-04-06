f07227ed52618e8814b29fae114ef8d9a3676733
Andressa Leal Cruz <andressa@take.net>
2018-04-06 18:30:41 -0300

    Change components export method

--- index.js
+++ index.js
@@ -3,13 +3,23 @@ import { SwitchComponent } from './components/switch';
 import { ContentTabsComponent } from './components/contentTabs';
 import { TabComponent } from './components/contentTabs/tab';
 import { PageHeaderDirective } from './components/pageHeader/PageHeaderDirective';
+import DatePicker from './components/datePicker/datePicker';
+import DatePickerRange from './components/datePicker/datePickerRange';
 
 (function(angular){
     angular
-        .module('blip.components', [])
-        .component('card', CardComponent)
+        .module('blip.components.card', [])
+        .component('card', CardComponent);
+    angular
+        .module('blip.components.switch', [])
         .component('switch', SwitchComponent)
-        .component('contentTabs', ContentTabsComponent)
-        .component('tab', TabComponent)
+    angular
+        .module('blip.components.contentTabs', [])
+        .component('contentTabs', ContentTabsComponent);
+    angular
+        .module('blip.components.tab', [])
+        .component('tab', TabComponent);
+    angular
+        .module('blip.components.pageHeaderDirective', [])
         .directive('pageHeader', PageHeaderDirective.factory);
 })(angular);
