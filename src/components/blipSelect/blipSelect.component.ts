6f3414d3a80466f2615c7cb67c777887921037ce
samwx <samuelmartins.sw@gmail.com>
2019-11-20 16:51:53 -0300

    feat(component): add new blip-select options and updated blip-toolkit
    master

--- src/components/blipSelect/blipSelect.component.ts
+++ src/components/blipSelect/blipSelect.component.ts
@@ -35,6 +35,7 @@ class BlipSelectController extends ComponentController
     clearAfterAdd: boolean;
     disabled: boolean;
     invalid: boolean;
+    descriptionPosition: string;
     //Callbacks
     onBeforeOpenSelect: () => void;
     onAfterOpenSelect: () => void;
@@ -63,6 +64,7 @@ class BlipSelectController extends ComponentController
             mode: this.mode || 'select',
             disabled: this.disabled || false,
             invalid: this.invalid || false,
+            descriptionPosition: this.descriptionPosition || 'right',
             onBeforeOpenSelect: this.handle.bind(
                 this,
                 BlipSelectCallback.OnBeforeOpenSelect,
@@ -281,6 +283,7 @@ export const BlipSelectComponent = angular
             label: '@?',
             placeholder: '@?',
             mode: '@?',
+            descriptionPosition: '@?',
             onBeforeOpenSelect: '&?',
             onAfterOpenSelect: '&?',
             onBeforeCloseSelect: '&?',
