6d3a9d0a993187e83194c7a5e3e38e8e0d8d7907
Vinicius Oliveira Souza <viniciusos@take.net>
2020-07-23 10:34:27 -0300

    feat(component): add sanitization on auto-expand directive
    feature/178777_sanitize_input

--- src/modules/index.ts
+++ src/modules/index.ts
@@ -1,7 +1,8 @@
 import angular from 'core/angular';
 import { translateModule } from './translate';
 import 'angular-tooltips';
+import 'angular-sanitize';
 
 export const SharedModule = angular
-    .module('SharedModules', ['720kb.tooltips', translateModule])
+    .module('SharedModules', ['ngSanitize', '720kb.tooltips', translateModule])
     .name;
