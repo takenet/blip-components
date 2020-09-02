5b3f25929a07911b04c4b1482ff290de46d06b8c
Vinicius Oliveira Souza <viniciusos@take.net>
2020-09-02 16:34:40 -0300

    feat(component): change input sanitize library
    bugfix/199677_ssml_sanitize

--- src/directives/inputSanitize/inputSanitize.directive.ts
+++ src/directives/inputSanitize/inputSanitize.directive.ts
@@ -1,35 +1,70 @@
 import angular from 'core/angular';
-import { ISCEService } from 'angular';
 import he from 'he';
+import * as sanitizeHtml from 'sanitize-html';
 
 class InputSanitize {
     public restrict: string;
     public require: string;
-    private typesToSanitize = ['text', 'password', 'email', 'search', 'url', 'textarea'];
+    private readonly TYPES_TO_SANITIZE = [
+        'text',
+        'password',
+        'email',
+        'search',
+        'url',
+        'textarea'
+    ];
+    private readonly SSML_TAGS = [
+        'speak',
+        'break',
+        'say-as',
+        'audio',
+        'p',
+        's',
+        'sub',
+        'mark',
+        'prosody',
+        'emphasis',
+        'par',
+        'seq',
+        'media',
+        'lang',
+        'phoneme',
+        'w',
+        'voice',
+        'lexicon',
+        'lookup',
+        'lexeme',
+        'grapheme'
+    ];
 
-    constructor(private $sce: ISCEService) {
+    constructor() {
         this.restrict = 'E';
         this.require = '?ngModel';
     }
 
     link(scope, element, attrs, ngModel) {
         if (ngModel) {
-            ngModel.$parsers.push((value) => {
-                if (this.typesToSanitize.includes(element[0].type)) {
-                    return he.decode(this.$sce.getTrustedHtml(value));
+            ngModel.$parsers.push(value => {
+                if (this.TYPES_TO_SANITIZE.includes(element[0].type)) {
+                    return he.decode(
+                        sanitizeHtml(value, {
+                            allowedTags: sanitizeHtml.defaults.allowedTags.concat(
+                                this.SSML_TAGS
+                            )
+                        })
+                    );
                 }
                 return value;
             });
         }
     }
 
-    static factory($sce) {
-        return new InputSanitize($sce);
+    static factory() {
+        return new InputSanitize();
     }
 }
 
 export const InputSanitizeDirective = angular
     .module('blipComponents.InputSanitize', [])
     .directive('input', InputSanitize.factory)
-    .directive('textarea', InputSanitize.factory)
-    .name;
+    .directive('textarea', InputSanitize.factory).name;
