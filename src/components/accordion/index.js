18c3d1ac67791a99a390dde976de1d93745a1c21
michael haruo nakashima <mhnakashima@gmail.com>
2021-05-05 16:51:49 -0300

    feat(accordion): update accordion - #232940

--- src/components/accordion/index.js
+++ src/components/accordion/index.js
@@ -1,34 +1,30 @@
 import angular from "core/angular";
+import template from './AccordionView.html';
 import "./Accordion.scss";
 class AccordionComponentController {
     imageuri;
     showimage;
     ischecked;
+    item;
 
     onFailure() {
         this.imageuri = undefined;
         this.showimage = undefined;
         this.ischecked = true;
+        this.item = undefined;
     }
 }
 
 export const accordion = angular
     .module("blipComponents.accordion", [])
     .component("accordion", {
-        template: `
-        <article class="accordion relative">
-            <input class="accordion-checkbox" type="checkbox" ng-checked="$ctrl.ischecked" />
-            <i class="accordion-chevron fr"></i>
-            <img ng-if="$ctrl.showimage" class="accordion-image icon icon-avatar w2 h2 round small" ng-src="{{$ctrl.imageuri}}" />
-            <h4 class="accordion-title ma0 ttu" ng-bind="$ctrl.title"></h4>
-            <div class="accordion-content relative overflow-hidden" ng-transclude></div>
-        </article>
-        `,
+        template,
         controller: AccordionComponentController,
         controllerAs: "$ctrl",
         transclude: true,
         bindings: {
             title: "@",
+            item: "<",
             imageuri: "<",
             showimage: "<",
             ischecked: "<"
